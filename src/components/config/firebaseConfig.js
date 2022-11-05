354925e3d83774cc2e7b39538dbc3769120e00d6
Meraz Meraz <devmeraz87@gmail.com>
2022-11-05 23:31:25 +0600

    mafad

--- src/components/config/firebaseConfig.js
+++ src/components/config/firebaseConfig.js
@@ -6,14 +6,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAVY0riRsRidtHrkoa_o2gGy-ApGgTUlE0",
-  authDomain: "new-chaking.firebaseapp.com",
-  projectId: "new-chaking",
-  storageBucket: "new-chaking.appspot.com",
-  messagingSenderId: "404110901647",
-  appId: "1:404110901647:web:076321849728db5d7ccbfa"
+  apiKey: "AIzaSyD09KIWmOlCrtWW4bI_xKaJBv3-K6YgsPs",
+  authDomain: "articles-50541.firebaseapp.com",
+  projectId: "articles-50541",
+  storageBucket: "articles-50541.appspot.com",
+  messagingSenderId: "251911638145",
+  appId: "1:251911638145:web:49f9e4abc5700f12745180"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
